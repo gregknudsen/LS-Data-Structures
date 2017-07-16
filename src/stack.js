a01b22bb041902a29c31fb4f6c6324c3cdc4e2bf
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-15 22:22:19 -0400

    almost done with stack...can't get size to work

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,22 @@
 class Stack {
+  constructor(size = 0) {
+    this.store = [];
+    this.size = size;
+  }
 
+  add(element) {
+    return this.store.push(element);
+    // return this.store.size += 1;
+  }
+
+  remove() {
+    return this.store.pop();
+    // return this.store.size -= 1;
+  }
+
+  size(store) {
+    return this.store.length;
+  }
 }
 
 module.exports = Stack;
