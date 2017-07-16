e3abc42b1ef8c9a3590f91d50470d2c192f78d77
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-15 22:55:12 -0400

    Queue functional...very similar to Stack with the first test not passing

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,20 @@
 class Queue {
+  constructor(size = 0) {
+    this.store = [];
+    this.size = size;
+  }
 
+  enqueue(element) {
+    this.size += 1;
+    return this.store.unshift(element);
+  }
+
+  dequeue() {
+    if (this.size > 0) {
+      this.size -= 1;
+    } else this.size = 0;
+    return this.store.pop();
+  }
 }
 
 module.exports = Queue;
